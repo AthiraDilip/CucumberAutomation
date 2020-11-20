df09ad49933923bffcf81a446ba9a9cb87b181c4
Athira Dilip <athiradilip90@gmail.com>
2020-11-20 06:32:58 +0530

    Adding cucumber based bdd automation framework

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,19 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"features"},
+        glue = {"steps"},
+        plugin = {"pretty", "html:Report1", "json:Report2", "junit:Report3"},
+//        dryRun = true
+//        strict = true,
+//        monochrome = true
+        tags = {"@test1 or @test2 or @test3 or @test4 or @test5 or @test6 or @test7" }
+//        name = {"Logo"}
+)
+public class TestRunner {
+}
